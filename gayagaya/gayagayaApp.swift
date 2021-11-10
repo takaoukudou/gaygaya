59ecc9b8da1823e458a6b12823a913e59673397b
takaoukudou <cu.bronze82@gmail.com>
2021-11-10 19:33:29 +0900

    first commit

---
+++ gayagaya/gayagayaApp.swift
@@ -0,0 +1,17 @@
+//
+//  gayagayaApp.swift
+//  gayagaya
+//
+//  Created by 工藤貴央 on 2021/11/10.
+//
+
+import SwiftUI
+
+@main
+struct gayagayaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
